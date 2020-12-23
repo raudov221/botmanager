8db18ce53cc4f40dfc4e957957b22ca620b56c4f
raudov221 <76117378+raudov221@users.noreply.github.com>
2020-12-23 19:51:18 +0700

    Update ok.py

--- ok.py
+++ ok.py
@@ -118,9 +118,9 @@ async def wrapper(ans: Message, sum, stak):
         random.choice(1,5) 
         if int(stak) == random.choice:
             await ans('ема ты молодец выйграл')
-            data["balance"][str(ans.from_id)] = int(data["balance"][str(ans.from_id] + int(sum)
+            data[ "balance" ][ str( ans.from_id ) ] = int( data[ "balance" ][ str( ans.from_id ) ] ) + int(sum)
         else:
             await ans('ты проиграл удача не на твоей стороне.') 
-            data["balance"][str(ans.from_id)] = int(data["balance"][str(ans.from_id] - int(sum)
+            data["balance"][str(ans.from_id)] = int(data["balance"][str(ans.from_id) ]) - int(sum)
 
 bot.run_polling( skip_updates = False )
