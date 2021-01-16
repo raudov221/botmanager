0ca1d53d6ebe091afacb2389b66a9755bb107891
raudov221 <76117378+raudov221@users.noreply.github.com>
2021-01-16 17:28:37 +0300

    Update ok.py

--- ok.py
+++ ok.py
@@ -1,4 +1,4 @@
-from vkbottle import Bot, Message, Mailing
+from vkbottle import Bot, Message, keyboard_gen, Branch, ExitBranch, Mailing
 bot = Bot('1ae80fd1fc2e59106902a96597761f4802b05b1787c147dbafef93e57b76447f8ea735c2af528562616d0', group_id, debug=True)
 mailing = Mailing('api_82735_N1cGUGYqt2f3oPL5YHFoLSSM', bot) # токен бери из настроек приложения 
 group_id = 200759417
