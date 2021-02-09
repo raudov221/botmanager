459dd560c68cf3a0dcae479061a6cf2a3a6d3893
raudov221 <76117378+raudov221@users.noreply.github.com>
2021-02-09 16:43:14 +0300

    Update ok.py

--- ok.py
+++ ok.py
@@ -21,6 +21,12 @@ async def wrapper(ans: Message, da, net: str):
     
 @user.on.message_handler(text="наградить медалью <da>")
 async def wrapper(ans: Message, da: str):
+    if int(da) == "vto.pe":
+        return "Такая магия на меня не действует!"
+    if int(da) == "https://vto.pe":
+        return "Такая магия на меня не действует!"
+    if int(da) == "http://vto.pe":
+        return "Такая магия на меня не действует!"
     return f"🌿 [id{ans.from_id}|Пользователь], наградил [id{ans.reply_message.from_id}|вас] медалью {da}"
 
 @user.on.message_handler(text="Наградить медалью <da>")
