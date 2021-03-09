b9bc8a8ae6df4d00942df1605868a489a2ba6a5b
raudov221 <76117378+raudov221@users.noreply.github.com>
2021-03-09 22:58:57 +0700

    Update ok.py

--- ok.py
+++ ok.py
@@ -99,11 +99,12 @@ async def wrapper(ans: Message, da: str):
         f.write(da)
         return f"b = da"
 
-@user.on.message_handler(text="кто")
-async def wrapper(ans: Message):
+@user.on.message_handler(text="кто <da>")
+async def wrapper(ans: Message, da: str):
     penis = await user.api.users.get(user_ids=ans.reply_message.from_id, fields='is_closed')
-    friends = await user.api.friends.get(user_id=ans.reply_message.from_id)
-    return f"🌿 Пользователь это [id{ans.reply_message.from_id}|{penis[0].first_name} {penis[0].last_name}]\n\n🥳 В сети: {penis[0].is_closed}\n😎 Кол-во друзей: {friends.count}"
+    users = await user.api.messages.get_conversation_members(peer_id=ans.peer_id)
+    return f'🌀 [id{ans.from_id}|{penis[0].first_name}], я думаю что {da} @id{random.choice([member.id for member in users.profiles if member.id])} (он)!'
+
 
 @user.on.message_handler(text="стикеры")
 async def wrapper(ans: Message):
@@ -200,100 +201,4 @@ async def wrapper(ans: Message, da: str):
             if a == 9:
                 return "у твоего отца пол сантимнтра по этому твоя мать лягушка, сука она кричала от боли моего пол метрового столба"
             if a == 10:
-                return "что-что? что ты там пищиж мразь я блять таких как ты по 10 штук в день обоссываю а ты ещё возникаешь хуепутало, если тебе не нравится мой хуй не соси ето же ты кричала эй эй дядя дайте хуй пососать а я вам сотку дам блять токо прикол в том чо сотку я тебе не отдам такчто ты пойдешь просто нахуй поэтому соси пока есть возможность выбиться в люди"
-            if a == 11:
-                return f"- знаешь почему у твоей мамы всегда зубы свежие?\n- потому что мой хуй со вкусом тик така"
-            if a == 12:
-                return "угадай кто сегодня идет в кинотеатр с твоей матерью"
-            if a == 13:
-                return "имбицил ебаный хватит хуи сосать внатуре"
-            if a == 14:
-                return "научи свою мать не сосать хуй, если сам умеешь"
-            if a == 15:
-                return "тебе мудопиздюку не раз было сказано не сосать хуй"
-            if a == 16:
-                return "моргенштерн топ"
-            if a == 17:
-                return "бравл старс топ"
-            if a == 18:
-                return "аниме говно, Аллах топ"
- 
-@user.on.message_handler(text="Обнять @<da>")
-async def wrapper(ans: Message, da: str):
-    return f"🤗 [id{ans.from_id}|Пользователь], обнял [{da}|вас] =)"
-
-@user.on.message_handler(text="трахнуть")
-async def wrapper(ans: Message):
-    return f"🤗 [id{ans.from_id}|Пользователь], трахнул [id{ans.reply_message.from_id}|вас] =)"
-
-@user.on.message_handler(text="Трахнуть")
-async def wrapper(ans: Message):
-    return f"🤗 [id{ans.from_id}|Пользователь], трахнул [id{ans.reply_message.from_id}|вас] =)"
-
-@user.on.message_handler(text="инфа <da>")
-async def wrapper(ans: Message, da: str):
-    random1 = random.randint(1, 100)
-    return f"🤗 [id{ans.from_id}|Пользователь], я думаю что {da} - {random1}%"
-
-@user.on.message_handler(text="Инфа <da>")
-async def wrapper(ans: Message, da: str):
-    random1 = random.randint(1, 100)
-    return f"🤗 [id{ans.from_id}|Пользователь], я думаю что {da} - {random1}%"
-
-@user.on.message_handler(text="бот <da>")
-async def wrapper(ans: Message, da: str):
-    return f"🤗 [id{ans.from_id}|Пользователь], привет"
-
-@user.on.message_handler(text="Бот <da>")
-async def wrapper(ans: Message, da: str):
-    return f"🤗 [id{ans.from_id}|Пользователь], привет"
-
-@user.on.message_handler(text="Иван <da>")
-async def wrapper(ans: Message, da: str):
-    return f"🤗 [id{ans.from_id}|Пользователь], привет"
-
-@user.on.message_handler(text="<da> иван")
-async def wrapper(ans: Message, da: str):
-    return f"🤗 [id{ans.from_id}|Пользователь], привет"
-
-@user.on.message_handler(text="<da> Иван")
-async def wrapper(ans: Message, da: str):
-    return f"🤗 [id{ans.from_id}|Пользователь], привет"
-
-@user.on.message_handler(text="<da> бот")
-async def wrapper(ans: Message, da: str):
-    return f"🤗 [id{ans.from_id}|Пользователь], привет"
-
-@user.on.message_handler(text="<da> Бот")
-async def wrapper(ans: Message, da: str):
-    return f"🤗 [id{ans.from_id}|Пользователь], привет"
-
-@user.on.message_handler(text="Команды")
-async def wrapper(ans: Message, da: str):
-    return f"🤗 [id{ans.from_id}|Пользователь], все команды доступны по этой ссылке:\n\nvk.com/@579018447-komandy"
-
-@user.on.message_handler(text="<gay>")
-async def wrapper(ans: Message, gay: str):
-    random1 = random.randint(1, 5)
-    random5 = random.randint(1, 100)
-    asa = 0
-    if random5 == 100:
-        asa = 100
-    if asa == 100:
-        if random1 == 1:
-            return "- Почему ваша кошка по субботам орет?\n- А мы ее моем.\n- Мы свою тоже моем по субботам...\n- А вы выжимать пробовали?"
-            asa = 0
-        if random1 == 2:
-            return "- Почему в мультфильме Маша и Медведь не показывают родителей Маши?\n- Они, наверное, уже в дурдоме!"
-            asa = 0
-        if random1 == 3:
-            return "Нетрезвый житель Ухрюпинска замахнулся на святое, но батюшка ударил раньше."
-            asa = 0
-        if random1 == 4:
-            return "Ребята, сделайте меня пожалуйста замом министра чего угодно, мне чисто ипотеку закрыть и всё, я дальше сам уволюсь, обещаю. Я всё посчитал, чтобы закрыть ипотеку мне на таком посту потребуется 17 секунд."
-            asa = 0
-        if random1 == 5:
-            return "Достаю из холодильника двухлитровую колу, директор: о, и мне налей!... и как объяснить, что она с вискарем уже?"
-            asa = 0
-    
-user.run_polling()
+                return "что-что? что ты там пищиж мразь я блять таких как ты по 10 штук в день обоссываю а ты ещё возникаешь хуепутало, если тебе не нравится мой хуй не соси ето же ты кричала э
