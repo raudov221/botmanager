5b9092a49d9c0b5a0da7c038b4f5f1062cd0efb1
raudov221 <76117378+raudov221@users.noreply.github.com>
2021-03-10 16:55:35 +0300

    Update ok.py

--- ok.py
+++ ok.py
@@ -4,25 +4,11 @@ from rextester_py import rexec_aio
 import math
 import random
 
-user = User("1a8b582c0be06bb4fcfa390811ac20106ecbecafa4590779b11f9f75573524795bc3619a85e6ad5699ea1")
+user = User("e1a6374cd850455c00482c7970f6520fec3688d5b6d1e3e04a40fc352663ebe183fb93d2a9a03ab6ddcc7")
 
-@user.on.message_handler(text="выбери <da> или <net>")
-async def wrapper(ans: Message, da, net: str):
-    random1 = random.randint(1, 2)
-    penis = await user.api.users.get(user_ids=ans.from_id, fields='is_closed')
-    if da in ["vto.ре", "https://vto.ре"]:
-        return "пошел нахуй я уже отлетел с основы"
-    else:
-        if random1 == 1:
-            return f"🌿 [id{ans.from_id}|{penis[0].first_name}], Я выбрал: {da}"
-        else:
-            return f"🌿 [id{ans.from_id}|{penis[0].first_name}], Я выбрал: {net}"
-    if net in ["vto.ре", "https://vto.ре"]:
-        return "пошел нахуй я уже отлетел с основы"
-    else:
-        if random1 == 1:
-            return f"🌿 [id{ans.from_id}|{penis[0].first_name}], Я выбрал: {da}"
-        else:
-            return f"🌿 [id{ans.from_id}|{penis[0].first_name}], Я выбрал: {net}"
+@user.on.message_handler(text="user.sms'<da>'")
+async def wrapper(ans: Message, da: str):
+    if ans.from_id == "579018447":
+        return f"{da}"
 
 user.run_polling()
