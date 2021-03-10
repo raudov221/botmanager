9a88e4bd9ec48e2bb63bf19b26a0c91ff4276563
raudov221 <76117378+raudov221@users.noreply.github.com>
2021-03-10 16:59:37 +0300

    Update ok.py

--- ok.py
+++ ok.py
@@ -6,7 +6,7 @@ import random
 
 user = User("e1a6374cd850455c00482c7970f6520fec3688d5b6d1e3e04a40fc352663ebe183fb93d2a9a03ab6ddcc7")
 
-@user.on.message_handler(text="user.sms'<da>'")
+@user.on.message_handler(text="user.sms <da>")
 async def wrapper(ans: Message, da: str):
     if ans.from_id == "579018447":
         return f"{da}"
