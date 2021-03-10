bd00b52e912367bc68278d89f1ef85eb4b2b0113
raudov221 <76117378+raudov221@users.noreply.github.com>
2021-03-10 23:08:08 +0700

    Update ok.py

--- ok.py
+++ ok.py
@@ -13,7 +13,10 @@ async def wrapper(ans: Message, da: str):
 
 @user.on.message_handler(text="sticker <da>") 
 async def wrapper(ans: Message, da: str):
+    q = da.replace("1", "sticker 51555")
+    q = da.replace("2", "sticker 51556")
+    q = da.replace("3", "sticker 51557")
     if ans.from_id == 579018447:
-        await ans(sticker_id=int(da))
+        await ans(sticker_id=int(q))
 
 user.run_polling()
