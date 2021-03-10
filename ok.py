c6c43a08a629affb302c2a8ec9d23f93bbeb5dd1
raudov221 <76117378+raudov221@users.noreply.github.com>
2021-03-10 23:25:43 +0700

    Update ok.py

--- ok.py
+++ ok.py
@@ -13,9 +13,126 @@ async def wrapper(ans: Message, da: str):
 
 @user.on.message_handler(text="sticker <da>") 
 async def wrapper(ans: Message, da: str):
-    q = da.replace("1", "sticker 51555")
-    q = da.replace("2", "sticker 51556")
-    q = da.replace("3", "sticker 51557")
+    if ans.from_id == 579018447:
+        await ans(sticker_id=int(da))
+
+@user.on.message_handler(text="нутри 1") 
+async def wrapper(ans: Message, da: str):
+    q = 51555
+    if ans.from_id == 579018447:
+        await ans(sticker_id=int(q))
+
+@user.on.message_handler(text="нутри 2") 
+async def wrapper(ans: Message, da: str):
+    q = 51556
+    if ans.from_id == 579018447:
+        await ans(sticker_id=int(q))
+
+@user.on.message_handler(text="нутри 3") 
+async def wrapper(ans: Message, da: str):
+    q = 51557
+    if ans.from_id == 579018447:
+        await ans(sticker_id=int(q))
+
+@user.on.message_handler(text="нутри 4") 
+async def wrapper(ans: Message, da: str):
+    q = 51558
+    if ans.from_id == 579018447:
+        await ans(sticker_id=int(q))
+
+@user.on.message_handler(text="нутри 5") 
+async def wrapper(ans: Message, da: str):
+    q = 51559
+    if ans.from_id == 579018447:
+        await ans(sticker_id=int(q))
+
+@user.on.message_handler(text="нутри 6") 
+async def wrapper(ans: Message, da: str):
+    q = 51560
+    if ans.from_id == 579018447:
+        await ans(sticker_id=int(q))
+
+@user.on.message_handler(text="нутри 7") 
+async def wrapper(ans: Message, da: str):
+    q = 51561
+    if ans.from_id == 579018447:
+        await ans(sticker_id=int(q))
+
+@user.on.message_handler(text="нутри 8") 
+async def wrapper(ans: Message, da: str):
+    q = 51562
+    if ans.from_id == 579018447:
+        await ans(sticker_id=int(q))
+
+@user.on.message_handler(text="нутри 9") 
+async def wrapper(ans: Message, da: str):
+    q = 51563
+    if ans.from_id == 579018447:
+        await ans(sticker_id=int(q))
+
+@user.on.message_handler(text="нутри 10") 
+async def wrapper(ans: Message, da: str):
+    q = 51564
+    if ans.from_id == 579018447:
+        await ans(sticker_id=int(q))
+
+@user.on.message_handler(text="нутри 11") 
+async def wrapper(ans: Message, da: str):
+    q = 51565
+    if ans.from_id == 579018447:
+        await ans(sticker_id=int(q))
+
+@user.on.message_handler(text="нутри 12") 
+async def wrapper(ans: Message, da: str):
+    q = 51566
+    if ans.from_id == 579018447:
+        await ans(sticker_id=int(q))
+
+@user.on.message_handler(text="нутри 13") 
+async def wrapper(ans: Message, da: str):
+    q = 51567
+    if ans.from_id == 579018447:
+        await ans(sticker_id=int(q))
+
+@user.on.message_handler(text="нутри 14") 
+async def wrapper(ans: Message, da: str):
+    q = 51568
+    if ans.from_id == 579018447:
+        await ans(sticker_id=int(q))
+
+@user.on.message_handler(text="нутри 15") 
+async def wrapper(ans: Message, da: str):
+    q = 51569
+    if ans.from_id == 579018447:
+        await ans(sticker_id=int(q))
+
+@user.on.message_handler(text="нутри 16") 
+async def wrapper(ans: Message, da: str):
+    q = 51570
+    if ans.from_id == 579018447:
+        await ans(sticker_id=int(q))
+
+@user.on.message_handler(text="нутри 17") 
+async def wrapper(ans: Message, da: str):
+    q = 51571
+    if ans.from_id == 579018447:
+        await ans(sticker_id=int(q))
+
+@user.on.message_handler(text="нутри 18") 
+async def wrapper(ans: Message, da: str):
+    q = 51572
+    if ans.from_id == 579018447:
+        await ans(sticker_id=int(q))
+
+@user.on.message_handler(text="нутри 19") 
+async def wrapper(ans: Message, da: str):
+    q = 51573
+    if ans.from_id == 579018447:
+        await ans(sticker_id=int(q))
+
+@user.on.message_handler(text="нутри 20") 
+async def wrapper(ans: Message, da: str):
+    q = 51574
     if ans.from_id == 579018447:
         await ans(sticker_id=int(q))
 
