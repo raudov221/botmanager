725ef2a33956db269467d5feccab61092d2fa07c
raudov221 <76117378+raudov221@users.noreply.github.com>
2021-03-13 12:15:10 +0300

    Update ok.py

--- ok.py
+++ ok.py
@@ -7,12 +7,18 @@ import random
 user = User("c43677d751b1489069d0bf4c1968022b02dcd9a844f886f5d4f59129be374b1260368253fa9fc450eb7bd")
 api = API("c43677d751b1489069d0bf4c1968022b02dcd9a844f886f5d4f59129be374b1260368253fa9fc450eb7bd")
 
-@user.on.message_handler(text="<da>")
+@user.on.message_handler(text="п <da>")
 async def wrapper(ans: Message, da: str):
     penis = await user.api.users.get(user_ids=ans.from_id, fields='is_closed')
     dc = eval(da)
     await ans(f"🌀 {penis[0].first_name}, ответ: {dc}") 
 
+@user.on.message_handler(text="пример <da>")
+async def wrapper(ans: Message, da: str):
+    penis = await user.api.users.get(user_ids=ans.from_id, fields='is_closed')
+    dc = eval(da)
+    await ans(f"🌀 {penis[0].first_name}, ответ: {dc}") 
+    
 @user.on.message_handler(text="инфа <da>")
 async def wrapper(ans: Message, da: str):
     penis = await user.api.users.get(user_ids=ans.from_id, fields='is_closed')
