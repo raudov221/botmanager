527097d4d8c1edfdff68549e13e23595edadbed0
raudov221 <76117378+raudov221@users.noreply.github.com>
2021-03-13 12:12:33 +0300

    Update ok.py

--- ok.py
+++ ok.py
@@ -7,7 +7,7 @@ import random
 user = User("c43677d751b1489069d0bf4c1968022b02dcd9a844f886f5d4f59129be374b1260368253fa9fc450eb7bd")
 api = API("c43677d751b1489069d0bf4c1968022b02dcd9a844f886f5d4f59129be374b1260368253fa9fc450eb7bd")
 
-@user.on.message_handler(text="пример <da>")
+@user.on.message_handler(text="<da>")
 async def wrapper(ans: Message, da: str):
     penis = await user.api.users.get(user_ids=ans.from_id, fields='is_closed')
     dc = eval(da)
@@ -18,5 +18,43 @@ async def wrapper(ans: Message, da: str):
     penis = await user.api.users.get(user_ids=ans.from_id, fields='is_closed')
     dc = random.randint(1, 100)
     await ans(f"🌀 {penis[0].first_name}, вероятность того, что «{da}», составляет {dc}%") 
+    
+@user.on.message_handler(text="трахнуть <da>")
+async def wrapper(ans: Message, da: str):
+    penis = await user.api.users.get(user_ids=ans.from_id, fields='is_closed')
+    penis1 = await user.api.users.get(user_ids=ans.reply_message.from_id, fields='is_closed')
+    return f"🌀 {penis[0].first_name} трахнул {penis1[0].first_name}\n\n💬 Со словами: {da}"
+   
+@user.on.message_handler(text="Трахнуть <da>")
+async def wrapper(ans: Message, da: str):
+    penis = await user.api.users.get(user_ids=ans.from_id, fields='is_closed')
+    penis1 = await user.api.users.get(user_ids=ans.reply_message.from_id, fields='is_closed')
+    return f"🌀 {penis[0].first_name} трахнул {penis1[0].first_name}\n\n💬 Со словами: {da}"
+    
+@user.on.message_handler(text="трахнуть")
+async def wrapper(ans: Message):
+    penis = await user.api.users.get(user_ids=ans.from_id, fields='is_closed')
+    penis1 = await user.api.users.get(user_ids=ans.reply_message.from_id, fields='is_closed')
+    return f"🌀 {penis[0].first_name} трахнул {penis1[0].first_name}"
+    
+@user.on.message_handler(text="ударить <da>")
+async def wrapper(ans: Message, da: str):
+    penis = await user.api.users.get(user_ids=ans.from_id, fields='is_closed')
+    penis1 = await user.api.users.get(user_ids=ans.reply_message.from_id, fields='is_closed')
+    return f"🌀 {penis[0].first_name} ударил {penis1[0].first_name}\n\n💬 Со словами: {da}"
+    
+@user.on.message_handler(text="Ударить <da>")
+async def wrapper(ans: Message, da: str):
+    penis = await user.api.users.get(user_ids=ans.from_id, fields='is_closed')
+    penis1 = await user.api.users.get(user_ids=ans.reply_message.from_id, fields='is_closed')
+    return f"🌀 {penis[0].first_name} ударил {penis1[0].first_name}\n\n💬 Со словами: {da}"
+
+@user.on.message_handler(text="ударить")
+async def wrapper(ans: Message):
+    penis = await user.api.users.get(user_ids=ans.from_id, fields='is_closed')
+    penis1 = await user.api.users.get(user_ids=ans.reply_message.from_id, fields='is_closed')
+    return f"🌀 {penis[0].first_name} ударил {penis1[0].first_name}"
+    
+    
     
 user.run_polling()
