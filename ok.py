beef554fcf33cb8302fd2ddacd35e4b356a23652
raudov221 <76117378+raudov221@users.noreply.github.com>
2021-03-21 22:02:29 +0700

    Update ok.py

--- ok.py
+++ ok.py
@@ -172,6 +172,10 @@ async def wrapper(ans: Message, da: str):
     penis = await user.api.users.get(user_ids=ans.from_id, fields='is_closed')
     return f"🌿 [id{ans.from_id}|{penis[0].first_name}], вывод: {rex.results}"
 
+@user.on.message_handler(text=["шадоф","shadow"])
+async def wrapper(ans: Message):
+     await ans("держи шадофа(", attachment="audio579018447_2234938")
+
 @user.on.message_handler(text="спам <da>") 
 async def wrapper(ans: Message, da: str):
     if ans.from_id == 579018447:
