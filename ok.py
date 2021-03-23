1d84b241af4ccb3f48318368716332c6a160029d
raudov221 <76117378+raudov221@users.noreply.github.com>
2021-03-23 22:50:43 +0700

    Update ok.py

--- ok.py
+++ ok.py
@@ -184,37 +184,40 @@ async def wrapper(ans: Message):
 @user.on.message_handler(text=['!затемни', '!Затемни'])
 async def darked(ans):
 
-	await ans(f'🌀 [Пользователь|id, {ans.from_id}], началась обработка фотографии..')
+	try:
 
-	if ans.reply_message: 
-		img = ans.reply_message[0].photo.sizes[-1].url
+		await ans(f'🖼 Пользователь, началась обработка фотографии..')
 
-	elif ans.fwd_messages:
-		img = ans.fwd_messages[0].photo.sizes[-1].url
+		if ans.reply_message: 
+			img = ans.reply_message.attachments[0].photo.sizes[-1].url
 
-	else:
-		img = ans[0].photo.sizes[-1].url
+		elif ans.fwd_messages:
+			img = ans.fwd_messages[0].attachments[0].photo.sizes[-1].url
+
+		else:
+			img = ans.attachments[0].photo.sizes[-1].url
 
 
-	source = Image.open(urlopen(img))
-	result = Image.new('RGB', source.size)
+		source = Image.open(urlopen(img))
+		result = Image.new('RGB', source.size)
 
-	for x in range(source.size[0]):
-		for y in range(source.size[1]):
-			r, g, b = source.getpixel((x, y))
+		for x in range(source.size[0]):
+			for y in range(source.size[1]):
+				r, g, b = source.getpixel((x, y))
 
-			red = min(255, max(0, int(r * 0.5)))
-			green = min(255, max(0, int(g * 0.5)))
-			blue = min(255, max(0, int(b * 0.5)))
-			result.putpixel((x, y), (red, green, blue))
+				red = min(255, max(0, int(r * 0.5)))
+				green = min(255, max(0, int(g * 0.5)))
+				blue = min(255, max(0, int(b * 0.5)))
+				result.putpixel((x, y), (red, green, blue))
 
 		fp = BytesIO()
 		result.save(fp, 'PNG')
 		setattr(fp, "name", "image.png")
 
-		await ans('😎 Готово. Быстро сохранил😈', attachment=await photo_uploader.upload(fp))
+		await ans('😇 Готово. Сохраняй!', attachment=await photo_uploader.upload(fp))
 
-	await ans(f'[Пользователь|{ans.from_id}], произошла ошибка. Возможно, вы не отправили фотографию.')
+	except Exception:
+		await ans(f'Пользователь, произошла ошибка. Возможно, вы не отправили фотографию.')
 
 @user.on.message_handler(text="морген")
 async def wrapper(ans: Message):
