480b5b4e0fc3538ab4495bf45efa32b3bfbae268
raudov221 <76117378+raudov221@users.noreply.github.com>
2021-04-07 13:34:28 +0300

    Update ok.py

--- ok.py
+++ ok.py
@@ -1,7 +1,6 @@
 from vkbottle.user import User, Message
 
 user = User("6e733cda91da130b0d5b517e84e1e93214e1f7a4f6bb48cc7f5ce11db4762142b8177563792af4dee1f46")
-api = API("6e733cda91da130b0d5b517e84e1e93214e1f7a4f6bb48cc7f5ce11db4762142b8177563792af4dee1f46")
 
 @user.on.message_handler(text="я дура")
 async def darked(ans: Message):
