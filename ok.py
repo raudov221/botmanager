d7dcfb183bc72c121958b79f4502dbdbdaeef3d1
raudov221 <76117378+raudov221@users.noreply.github.com>
2021-05-11 17:26:51 +0300

    Update ok.py

--- ok.py
+++ ok.py
@@ -7,10 +7,9 @@ blocked = ["porno365", "porno", "xnxx", "vto", "vto.pe", "ru", "com", "pe"]
 
 user = User("1d461298f071fc440160706f4d6beb66ee1052a87090b001ee0c99aae13f0191f6a78d48c2c9f0509b426", mobile = True)
 
-@user.on.message_handler(text="@grizzlybot ✅ Принять", lower = True)
+@user.on.message_handler(text=".брак принять", lower = True)
 async def wrapper(ans: Message):
-	if ans.from_id == "542720500":
-		await ans(".брак пошалить")
+	await ans(".брак пошалить")
 
 
 user.run_polling()
