9c2d498c1e62f82de870654f99817d1b39774cd2
raudov221 <76117378+raudov221@users.noreply.github.com>
2021-05-30 10:35:22 +0300

    Update ok.py

--- ok.py
+++ ok.py
@@ -5,11 +5,12 @@ import random
 blocked = ["porno365", "porno", "xnxx", "vto", "vto.pe", "ru", "com", "pe"]
 
 
-user = User("f316473ce9ae540331f5fe8cbdc1bd1c7901324b1ca0b42648be820b589b00f17f7ccb94646b7d32cd9c2", mobile = True)
+user = User("f316473ce9ae540331f5fe8cbdc1bd1c7901324b1ca0b42648be820b589b00f17f7ccb94646b7d32cd9c2")
 
-@user.on.message_handler(text="привет <da>", lower = True)
-async def wrapper(ans: Message, da):
-	await ans("[id{ans.from_id}|пошел нахуй]")
+@user.on.message_handler(text=".иван <da> до <da1>", lower = True)
+async def wrapper(ans: Message, da, da1):
+	a = random.randint(da, da1)
+	await ans("вам выпало {a}")
 
 
 user.run_polling()
