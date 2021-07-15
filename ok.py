9bb10a8bd03f7a89c3a5cc0c757113d7e8753970
raudov221 <76117378+raudov221@users.noreply.github.com>
2021-07-15 13:55:55 +0300

    Update ok.py

--- ok.py
+++ ok.py
@@ -24,7 +24,11 @@ def reg( ans ):
         data[ "pets" ][ str( ans.from_id ) ] = "нету ты бомж"
         data[ "cars" ][ str( ans.from_id ) ] = "нету ты бомж"
         data[ "tyanka" ][ str( ans.from_id ) ] = "0"
-        data[ "timebonus" ][ str( ans.from_id ) ] = "0"
+        data[ "exp" ][ str( ans.from_id ) ] = 0
+        data[ "expnot" ][ str( ans.from_id ) ] = 50
+        data[ "rabot" ][ str( ans.from_id ) ] = 0
+        data[ "lvl" ][ str( ans.from_id ) ] = 0
+        data[ "timebonus" ][ str( ans.from_id ) ] = 0
         data[ "id" ][ str( ans.from_id ) ] = str( len( data[ "user" ] ) )
         json.dump( data, open( "data.json", "w" ) )
 
@@ -35,7 +39,7 @@ main.add_row()
 main.add_button( Text( label = "баланс" ), color = "positive" )
 main.add_button( Text( label = "бонус" ), color = "positive" )
 main.add_row()
-main.add_button( Text( label = "вложения" ), color = "default" )
+main.add_button( Text( label = "работы" ), color = "default" )
 main.add_button( Text( label = "клик" ), color = "default" )
 main.add_button( Text( label = "скам" ), color = "default" )
 main.add_row()
@@ -58,11 +62,23 @@ async def wrapper( ans: Message ):
 	data = json.load( open( "data.json", "r" ) )
 	await ans( f"добро пожаловать в бота!\n\nвой айди {data['id'][str(ans.from_id)]}", keyboard = main )
 
+@bot.on.message(text='<da>')
+async def wrapper(ans: Message, da):
+	data = json.load( open( "data.json", "r" ) )
+	if data['exp'][str(ans.from_id)] > data['expnot'][str(ans.from_id)]:
+		await ans(f"вы перешли на новый уровень!")
+		data["lvl"][str(ans.from_id)] +=1
+		data["exp"][str(ans.from_id)] = 0
+		lvlup = data['expnot'][str(ans.from_id)]
+		lvlup *2
+		data["expnot"][str(ans.from_id)] = lvlup
+		json.dump( data, open( "data.json", "w" ) )
+	
 @bot.on.message( text = [ "я" ], lower = True )
 async def wrapper( ans: Message ):
 	reg( ans )
 	data = json.load( open( "data.json", "r" ) )
-	await ans( f"твой айди: {data['id'][str(ans.from_id)]}", keyboard = main )
+	await ans( f"профиль: \n\nid - {data['id'][str(ans.from_id)]}\nопыт: {data['exp'][str(ans.from_id)]}/{data['expnot'][str(ans.from_id)]}\nlvl - {data['lvl'][str(ans.from_id)]}", keyboard = main )
 
 @bot.on.message( text = [ "баланс" ], lower = True )
 async def wrapper( ans: Message ):
@@ -88,17 +104,19 @@ async def wrapper( ans: Message ):
 	data = json.load( open( "data.json", "r" ) )
 	scam = random.randint(0, 2)
 	data["balance"][str(ans.from_id)] +=scam
+	data["exp"][str(ans.from_id)] +=1
+	json.dump( data, open( "data.json", "w" ) )
 	await ans(f"""вы заскамили бота!!!!!
 
 ваш баланс: {data['balance'][str(ans.from_id)]}""")
-	json.dump( data, open( "data.json", "w" ) )
 
 @bot.on.message( text = [ "клик" ], lower = True )
 async def wrapper( ans: Message ):
 	data = json.load( open( "data.json", "r" ) )
 	data["balance"][str(ans.from_id)] +=1
-	await ans(f"ты кликнул: {data['balance'][str(ans.from_id)]}")
+	data["exp"][str(ans.from_id)] +=1
 	json.dump( data, open( "data.json", "w" ) )
+	await ans(f"ты кликнул: {data['balance'][str(ans.from_id)]}")
 
 @bot.on.message( text = [ "пэты" ], lower = True )
 async def wrapper( ans: Message ):
@@ -155,66 +173,74 @@ async def wrapper( ans: Message ):
 @bot.on.message( text = [ "тянку купить 1" ], lower = True )
 async def wrapper( ans: Message ):
 	data = json.load( open( "data.json", "r" ) )
-	if data['balance'][str(ans.from_id)] > 100:
+	if data['balance'][str(ans.from_id)] < 100:
 		data[ "tyanka" ][ str( ans.from_id ) ] = "1"
 		data[ "balance" ][ str( ans.from_id ) ] -= 100
 		await ans(f"ты приобрел тянку!")
+		json.dump( data, open( "data.json", "w" ) )
 
 @bot.on.message( text = [ "тянку купить 2" ], lower = True )
 async def wrapper( ans: Message ):
 	data = json.load( open( "data.json", "r" ) )
-	if data['balance'][str(ans.from_id)] > 200:
+	if data['balance'][str(ans.from_id)] < 200:
 		data[ "tyanka" ][ str( ans.from_id ) ] = "2"
 		data[ "balance" ][ str( ans.from_id ) ] -= 200
 		await ans(f"ты приобрел тянку!")
+		json.dump( data, open( "data.json", "w" ) )
 
 @bot.on.message( text = [ "тянку купить 3" ], lower = True )
 async def wrapper( ans: Message ):
 	data = json.load( open( "data.json", "r" ) )
-	if data['balance'][str(ans.from_id)] > 500:
+	if data['balance'][str(ans.from_id)] < 500:
 		data[ "tyanka" ][ str( ans.from_id ) ] = "3"
 		data[ "balance" ][ str( ans.from_id ) ] -= 500
 		await ans(f"ты приобрел тянку!")
+		json.dump( data, open( "data.json", "w" ) )
 
 @bot.on.message( text = [ "кары купить 1" ], lower = True )
 async def wrapper( ans: Message ):
 	data = json.load( open( "data.json", "r" ) )
-	if data['balance'][str(ans.from_id)] > 1000:
+	if data['balance'][str(ans.from_id)] < 1000:
 		data[ "cars" ][ str( ans.from_id ) ] += "\nваз 2101"
 		data[ "balance" ][ str( ans.from_id ) ] -= 1000
 		await ans(f"ты приобрел машинк!")
+		json.dump( data, open( "data.json", "w" ) )
 
 @bot.on.message( text = [ "кары купить 2" ], lower = True )
 async def wrapper( ans: Message ):
 	data = json.load( open( "data.json", "r" ) )
-	if data['balance'][str(ans.from_id)] > 1200:
+	if data['balance'][str(ans.from_id)] < 1200:
 		data[ "cars" ][ str( ans.from_id ) ] += "\nваз 2103"
 		data[ "balance" ][ str( ans.from_id ) ] -= 1200
 		await ans(f"ты приобрел машинк!")
+		json.dump( data, open( "data.json", "w" ) )
 
 @bot.on.message( text = [ "кары купить 3" ], lower = True )
 async def wrapper( ans: Message ):
 	data = json.load( open( "data.json", "r" ) )
-	if data['balance'][str(ans.from_id)] > 2500:
+	if data['balance'][str(ans.from_id)] < 2500:
 		data[ "cars" ][ str( ans.from_id ) ] += "\nваз 2114"
 		data[ "balance" ][ str( ans.from_id ) ] -= 2500
 		await ans(f"ты приобрел машинк!")
+		json.dump( data, open( "data.json", "w" ) )
 
 @bot.on.message( text = [ "кары купить 4" ], lower = True )
 async def wrapper( ans: Message ):
 	data = json.load( open( "data.json", "r" ) )
-	if data['balance'][str(ans.from_id)] > 4000:
+	if data['balance'][str(ans.from_id)] < 4000:
 		data[ "cars" ][ str( ans.from_id ) ] += "\nмазда 6"
 		data[ "balance" ][ str( ans.from_id ) ] -= 4000
 		await ans(f"ты приобрел машинк!")
+		json.dump( data, open( "data.json", "w" ) )
 
 @bot.on.message( text = [ "кары купить 5" ], lower = True )
 async def wrapper( ans: Message ):
 	data = json.load( open( "data.json", "r" ) )
-	if data['balance'][str(ans.from_id)] > 8000:
+	if data['balance'][str(ans.from_id)] < 8000:
 		data[ "cars" ][ str( ans.from_id ) ] += "\nламбаргини"
 		data[ "balance" ][ str( ans.from_id ) ] -= 8000
 		await ans(f"ты приобрел машинк!")
+		json.dump( data, open( "data.json", "w" ) )
 
 @bot.on.message(text=["репорт <ff>"], lower = True)
 async def wrapper(ans: Message, ff):
@@ -246,4 +272,65 @@ async def lsmsg(ans: Message, txt):
         end_time = time.time()
         await ans(f'Рассылка завершена за {round(end_time-start_time, 1)} сек.')
 	
+@bot.on.message(text='работы')
+async def wrapper(ans: Message):
+	data = json.load( open( "data.json", "r" ) )
+	if int(data[ "lvl" ][ str( ans.from_id ) ]) > 0:
+		await ans(f"работы на которые можно устроиться:\n\nраздавать листовки - 10 монет за 1 листовку")
+	if int(data[ "lvl" ][ str( ans.from_id ) ]) > 2:
+		await ans(f"работы на которые можно устроиться:\n\nраздавать листовки - 10 монет за 1 листовку\nтаксист - 50 монет за 1 едьбу")
+	if int(data[ "lvl" ][ str( ans.from_id ) ]) > 3:
+		await ans(f"работы на которые можно устроиться:\n\nраздавать листовки - 10 монет за 1 листовку\nтаксист - 50 монет за 1 едьбу\nшахтер - 100 монет за 1 руду")
+	if int(data[ "lvl" ][ str( ans.from_id ) ]) > 5:
+		await ans(f"работы на которые можно устроиться:\n\nраздавать листовки - 10 монет за 1 листовку\nтаксист - 50 монет за 1 едьбу\nшахтер - 100 монет за 1 руду\nпрограммист - 500 монет за 1 код.")
+
+@bot.on.message(text='работы устроиться 1')
+async def wrapper(ans: Message):
+	data = json.load( open( "data.json", "r" ) )
+	if int(data[ "lvl" ][ str( ans.from_id ) ]) > 0:
+		data[ "rabota" ][ str( ans.from_id ) ] = 1
+		await ans(f"вы устроились на работу раздавать листовки")
+		json.dump( data, open( "data.json", "w" ) )
+
+@bot.on.message(text='работы устроиться 2')
+async def wrapper(ans: Message):
+	data = json.load( open( "data.json", "r" ) )
+	if int(data[ "lvl" ][ str( ans.from_id ) ]) > 2:
+		data[ "rabota" ][ str( ans.from_id ) ] = 2
+		await ans(f"вы устроились на работу таксист")
+		json.dump( data, open( "data.json", "w" ) )
+
+@bot.on.message(text='работы устроиться 3')
+async def wrapper(ans: Message):
+	data = json.load( open( "data.json", "r" ) )
+	if int(data[ "lvl" ][ str( ans.from_id ) ]) > 3:
+		data[ "rabota" ][ str( ans.from_id ) ] = 3
+		await ans(f"вы устроились на работу шахтер")
+		json.dump( data, open( "data.json", "w" ) )
+
+@bot.on.message(text='работы устроиться 4')
+async def wrapper(ans: Message):
+	data = json.load( open( "data.json", "r" ) )
+	if int(data[ "lvl" ][ str( ans.from_id ) ]) > 5:
+		data[ "rabota" ][ str( ans.from_id ) ] = 4
+		await ans(f"вы устроились на работу программист")
+		json.dump( data, open( "data.json", "w" ) )
+
+@bot.on.message(text='работать')
+async def wrapper(ans: Message):
+	data = json.load( open( "data.json", "r" ) )
+	if data[ "rabota" ][ str( ans.from_id ) ] == 1:
+		data[ "balance" ][ str( ans.from_id ) ] +=10
+		await ans(f"вы приклеили 1 листовку к столбу.\nбаланс:{data[ 'balance' ][ str( ans.from_id ) ]}")
+	if data[ "rabota" ][ str( ans.from_id ) ] == 2:
+		data[ "balance" ][ str( ans.from_id ) ] +=50
+		await ans(f"вы привезли 1 клиента.\nбаланс:{data[ 'balance' ][ str( ans.from_id ) ]}")
+	if data[ "rabota" ][ str( ans.from_id ) ] == 3:
+		data[ "balance" ][ str( ans.from_id ) ] +=100
+		await ans(f"вы добыли 1 руду.\nбаланс:{data[ 'balance' ][ str( ans.from_id ) ]}")
+	if data[ "rabota" ][ str( ans.from_id ) ] == 4:
+		data[ "balance" ][ str( ans.from_id ) ] +=500
+		await ans(f"вы написали 1 код.\nбаланс:{data[ 'balance' ][ str( ans.from_id ) ]}")
+	json.dump( data, open( "data.json", "w" ) )
+
 bot.run_polling( skip_updates = False )
