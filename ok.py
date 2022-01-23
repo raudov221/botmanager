df000a25a4cb35a9cf9da28e6efa9b022941b402
raudov221 <76117378+raudov221@users.noreply.github.com>
2022-01-23 13:07:11 +0300

    Update ok.py

--- ok.py
+++ ok.py
@@ -13,15 +13,8 @@ def reg( ans ):
         pass
     else:
         data[ "user" ][ str( ans.from_id ) ] = "reg"
-        data[ "balance" ][ str( ans.from_id ) ] = 0
-        data[ "pets" ][ str( ans.from_id ) ] = "нету ты бомж"
-        data[ "cars" ][ str( ans.from_id ) ] = "нету ты бомж"
-        data[ "tyanka" ][ str( ans.from_id ) ] = "0"
-        data[ "exp" ][ str( ans.from_id ) ] = 0
-        data[ "expnot" ][ str( ans.from_id ) ] = 50
-        data[ "rabot" ][ str( ans.from_id ) ] = 0
-        data[ "lvl" ][ str( ans.from_id ) ] = 0
-        data[ "timebonus" ][ str( ans.from_id ) ] = 0
+        data[ "on" ][ str( ans.from_id ) ] = False
+	data[ "sym" ][ str( ans.from_id ) ] = 0
         data[ "id" ][ str( ans.from_id ) ] = str( len( data[ "user" ] ) )
         json.dump( data, open( "data.json", "w" ) )
 
@@ -29,25 +22,28 @@ def reg( ans ):
 async def wrapper( ans: Message ):
 	reg( ans )
 	data = json.load( open( "data.json", "r" ) )
-	if data[ "pets" ][ str( ans.from_id ) ] == "1":
-		await ans( f"hash303191")
-		data[ "pets" ][ str( ans.from_id ) ] == "0"
+	if data[ "on" ][ str( ans.from_id ) ] == True:
+		await ans( f"бот успешно отключен.")
+		data[ "on" ][ str( ans.from_id ) ] == False
 	else: 
-		data[ "pets" ][ str( ans.from_id ) ] = "1"
-		await ans( f"spam = 1")
+		data[ "on" ][ str( ans.from_id ) ] = True
+		await ans( f"бот успешно включен.")
 	json.dump( data, open( "data.json", "w" ) )
+	
 	
 	
 @user.on.message_handler( text = [ "<da>" ], lower = True )
 async def wrapper( ans: Message, da ):
 	reg( ans )
 	data = json.load( open( "data.json", "r" ) )
-	if data[ "pets" ][ str( ans.from_id ) ] == "1":
+	if data[ "on" ][ str( ans.from_id ) ] == "1":
 		random1 = random.randint(1, 5)
 		if random1 == 5:
 			filename = 'hate.txt'
 			with open(filename) as f:
     				lines = f.readlines()
 				await ans(random.choice(lines))
+				data[ "sym" ][ str( ans.from_id ) ] += 1
+				json.dump( data, open( "data.json", "w" ) )
 	
 user.run_polling()
